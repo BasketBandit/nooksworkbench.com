adc8c2a0ab8c5a511dab8bba26ae6f5d7a1caaae
BasketBandit <errorsfound@hotmail.co.jp>
2020-04-24 00:33:55 +0100

    update db calls to sort requests alphabetically

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -12,21 +12,21 @@ class DefaultController extends Controller {
     public function __construct() {
         $this->categories = DB::table('recipe')->select('category')->groupBy('category')->get();
         $this->tags = DB::table('recipe')->select('tag')->groupBy('tag')->get();
-        $this->sources = DB::table('recipe')->selectRaw('substring_index(`source`, ", ", 1) as sources')->groupByRaw('sources')->get();
+        $this->sources = DB::table('recipe')->selectRaw('substring_index(`source`, ", ", 1) as sources')->groupBy('sources')->get();
      }
 
 	public function index() {
-		$recipes = DB::table('recipe')->paginate(24);
+		$recipes = DB::table('recipe')->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
 	}
 
 	public function browse($name) {
-        $recipes = DB::table('recipe')->where('name','LIKE','%'.$name."%")->paginate(24);
+        $recipes = DB::table('recipe')->where('name','LIKE','%'.$name."%")->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function recipe($name) {
-        $recipes = DB::table('recipe')->where('name','=',$name)->paginate(24);
+        $recipes = DB::table('recipe')->where('name','=',$name)->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
@@ -37,32 +37,33 @@ class DefaultController extends Controller {
                                       ->orWhere('m4_id','LIKE','%'.$name."%")
                                       ->orWhere('m5_id','LIKE','%'.$name."%")
                                       ->orWhere('m6_id','LIKE','%'.$name."%")
+                                      ->orderBy('name', 'asc')
                                       ->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function category($name) {
-        $recipes = DB::table('recipe')->where('category','=',$name)->paginate(24);
+        $recipes = DB::table('recipe')->where('category','=',$name)->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function size($name) {
-        $recipes = DB::table('recipe')->where('grid','=',$name)->paginate(24);
+        $recipes = DB::table('recipe')->where('grid','=',$name)->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function tag($name) {
-        $recipes = DB::table('recipe')->where('tag','LIKE','%'.$name.'%')->paginate(24);
+        $recipes = DB::table('recipe')->where('tag','LIKE','%'.$name.'%')->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function source($name) {
-        $recipes = DB::table('recipe')->where('source','LIKE','%'.$name.'%')->paginate(24);
+        $recipes = DB::table('recipe')->where('source','LIKE','%'.$name.'%')->orderBy('name', 'asc')->paginate(24);
 		return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 
     public function customisable($name) {
-        $recipes = DB::table('recipe')->where('customisable','=',$name)->paginate(24);
+        $recipes = DB::table('recipe')->where('customisable','=',$name)->orderBy('name', 'asc')->paginate(24);
     	return view('index')->with('categories', $this->categories)->with('tags', $this->tags)->with('sources', $this->sources)->with('recipes', $recipes);
     }
 }
