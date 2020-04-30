0248aed7660c454cc69af24e3e3f38c7f071fb76
BasketBandit <errorsfound@hotmail.co.jp>
2020-04-30 22:41:37 +0100

    fix search functions not working

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -87,7 +87,47 @@
         @include('layouts.footer')
     </footer>
 
-    <script src="{{ url('/js/search.js') }}" type="text/javascript"></script>
+    <script type="text/javascript">
+        $("#search").on('keypress',function(e) {
+            if(e.which === 13) {
+                window.location = "{{ $base_url }}/browse/" + $("#search").val().toLowerCase();
+            }
+        });
+
+        $("#search-addon").click(function() {
+            window.location = "{{ $base_url }}/browse/" + $("#search").val().toLowerCase();
+        });
+
+        $("#item-search").click(function() {
+            window.location = "{{ $base_url }}/browse/" + $("#search").val().toLowerCase();
+        });
+
+        $("#material-search").click(function() {
+            window.location = "{{ $base_url }}/material/" + $("#search").val().toLowerCase();
+        });
+
+        $("#tag-search").click(function() {
+            window.location = "{{ $base_url }}/tag/" + $("#search").val().toLowerCase();
+        });
+
+        $('#nav-category').click(function(){
+            $('#nav-extended-category').toggleClass('visible');
+            $('#nav-extended-tag').removeClass('visible');
+            $('#nav-extended-source').removeClass('visible');
+        });
+
+        $('#nav-tag').click(function(){
+            $('#nav-extended-tag').toggleClass('visible');
+            $('#nav-extended-category').removeClass('visible');
+            $('#nav-extended-source').removeClass('visible');
+        });
+
+        $('#nav-source').click(function(){
+            $('#nav-extended-source').toggleClass('visible');
+            $('#nav-extended-tag').removeClass('visible');
+            $('#nav-extended-category').removeClass('visible');
+        });
+    </script>
 </body>
 
 </html>
