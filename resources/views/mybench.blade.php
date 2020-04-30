32b901fc0cf7b40072e776cdc5e11304c6407e82
BasketBandit <errorsfound@hotmail.co.jp>
2020-04-30 12:33:15 +0100

    refactored a lot of code duplication using variable variables

--- resources/views/mybench.blade.php
+++ resources/views/mybench.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div id="content">
     @if(count($recipes) == 0)
-    <div class="alert alert-warning" role="alert">
-        It looks like you haven't unlocked any recipes yet {{ Auth::user()->username }}! <small>...{{ Auth::user()->username }}!</small>
-    </div>
+        <div class="alert alert-warning" role="alert">
+            It looks like you haven't unlocked any recipes yet {{ Auth::user()->username }}! <small>...{{ Auth::user()->username }}!</small>
+        </div>
     @endif
 
-    <div class="secondary-nav bg-pgreen" role="alert">
+    <div class="secondary-nav mb-1 bg-pgreen" role="alert">
         <a href="/mybench/settings"><button class="btn btn-nook">Settings</button></a>
     </div>
 
@@ -20,7 +20,7 @@
 
     <div class="row justify-content-center">
         @foreach($recipes as $recipe)
-            <div class="card col-md-6 text-left mb-2">
+            <div class="card col-md-6 text-left m-1">
                 <div class="row no-gutters h-100">
                     <div class="card-img-wrap col-md-4 p-2">
                         <a href="/mybench/recipe/{{ rawurlencode(strtolower($recipe->name)) }}">
@@ -32,102 +32,35 @@
                         </a>
                     </div>
 
-                    <div class="col w-100">
+                    <div class="col w-100 mr-1">
                         <div class="card-body text-left p-2 pt-3">
                             @if($recipe->customisable == 1)
                                 <a href="/mybench/customisable/1"><i class="fas fa-paint-brush mt-1 float-right"></i></a>
                             @endif
 
                             <h5 class="card-title text-center">{{ $recipe->name }}</h5>
-                            @if($recipe->m1_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m1_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m1_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m1_id).'.png') }}" alt="{{ $recipe->m1_id }}">
-                                            </picture>
+                            @for($i = 1; $i < 7; $i++)
+                                @php $id = "m".$i."_id"; $val = "m".$i."_val"; @endphp
+                                @if($recipe->$val > 0)
+                                    <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m1_id)) }}">
+                                        <div class="mat rounded w-100 p-1 mb-1">
+                                            <div class="mat-img">
+                                                <picture>
+                                                    <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->$id).'.webp') }}">
+                                                    <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->$id).'.png') }}" alt="{{ $recipe->$id }}">
+                                                </picture>
+                                            </div>
+                                            <div class="mat-txt d-inline-block align-middle">{{ $recipe->$id }}</div>
+                                            <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->$val }}</span></div>
                                         </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m1_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m1_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
-                            @if($recipe->m2_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m2_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m2_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m2_id).'.png') }}" alt="{{ $recipe->m2_id }}">
-                                            </picture>
-                                        </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m2_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m2_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
-                            @if($recipe->m3_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m3_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m3_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m3_id).'.png') }}" alt="{{ $recipe->m3_id }}">
-                                            </picture>
-                                        </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m3_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m3_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
-                            @if($recipe->m4_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m4_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m4_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m4_id).'.png') }}" alt="{{ $recipe->m4_id }}">
-                                            </picture>
-                                        </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m4_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m4_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
-                            @if($recipe->m5_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m5_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m5_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m5_id).'.png') }}" alt="{{ $recipe->m5_id }}">
-                                            </picture>
-                                        </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m5_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m5_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
-                            @if($recipe->m6_val > 0)
-                                <a href="/mybench/material/{{ rawurlencode(strtolower($recipe->m6_id)) }}">
-                                    <div class="mat rounded w-100 p-1 mb-1">
-                                        <div class="mat-img">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m6_id).'.webp') }}">
-                                                <img src="{{ secure_asset('/i/inventory/'.rawurlencode($recipe->m6_id).'.png') }}" alt="{{ $recipe->m6_id }}">
-                                            </picture>
-                                        </div>
-                                        <div class="mat-txt d-inline-block">{{ $recipe->m6_id }}</div>
-                                        <div class="mat-val"><span class="mat-val badge badge-nook">x{{ $recipe->m6_val }}</span></div>
-                                    </div>
-                                </a>
-                            @endif
+                                    </a>
+                                @endif
+                            @endfor
                         </div>
                     </div>
                 </div>
 
-                <div class="row ml-1 mr-1 mb-1 pl-1 pr-1">
+                <div class="row m-1 mr-2 ml-2">
                     <div class="text-center mr-1">
                         <a href="/mybench/category/{{ strtolower($recipe->category) }}"><button class="btn-sm btn-light mt-1 mb-1 text-center text-muted">{{ $recipe->category }}</button></a>
                     </div>
